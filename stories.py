5848ea48edaa0222fcb42d491137a821b00f1886
Mithreia <denisemk@mithreum.com>
2025-01-07 20:51:24 -0500

    Add files via upload

---
+++ stories.py
@@ -0,0 +1,50 @@
+love_sonnet_18 = """
+    Shall I compare thee to a {}'s {}?
+    Thou art more {} and more {}:
+    Rough winds do {} the darling {} of May,
+    And {};s lease hath all too {} a date:
+    Sometime too {} the eye of heaven shines,
+    And often is his {} complexion dimm'd;
+    And every fair from fair sometime declines,
+    By {} or nature's changing course untrimm'd;
+    But thy {} summer shall not {}
+    Nor lose possession of that {} thou owest;
+    Nor shall Death {} thou wander'st in his {},
+    When in eternal {} to time thou growest:
+    So long as {} can {} or {} can {},
+    So long lives this and this gives life to thee.
+    
+        - William Shakespeare-ish?
+        
+    """
+the_magic_flute = """
+    Prince Tamino escapes a {} {} and arrives in {}.
+    He meets the bird catcher Papageno, and they travel to
+    Sarastro's {} to rescue the princess Pamina. For protection,
+    Tamino brings a {} {}, which he plays to tame {} {}s. Tamino
+    must prove that he is worthy to marry the princess, but 
+    Papageno says that he doesn't need love: He's happy with just
+    a glass of {} and plate of {}. After successfully using the {}
+    to pass safely through fire and water, Tamino and Pamina are 
+    reunited in the {}, and everyone celebrates the power of true love!
+    """
+
+one_more_game = """
+    Convincing yor {} to let you stay up past your bedtime to play video games can be {},
+    but it's not impossible. Here are some {} excuses to use when you need one last game.
+
+    - My {} hurts. The only way it'll feel better is if I {} these cyborgs and save (the)
+    {}. 
+
+    - 
+"""
+
+dict_of_stories = {love_sonnet_18:
+                   ['season', 'noun', 'adjective', 'adjective','verb',
+                    'plural noun', 'season', 'adjective', 'adjective', 'adjective',
+                    'noun', 'adjective', 'verb', 'noun', 'verb', 'noun',
+                    'plural noun', 'noun', 'verb', 'noun', 'verb'],
+                    the_magic_flute:
+                    ['color', 'type of animal', 'location', 'type of building',
+                     'adjective', 'musical instrument', 'number','noun', 'second type of animal',
+                     'food', 'musical instrument', 'room in castle']}
